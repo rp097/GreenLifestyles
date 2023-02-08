875349fa0586b6cc7a873af39d96808a63031730
rp097 <rahulprem90@gmail.com>
2023-02-08 17:43:31 +1100

    First Test Case

--- test/test.js
+++ test/test.js
@@ -1,17 +1,19 @@
 var expect  = require("chai").expect;
 var request = require("request");
 
-describe("Add Two Numbers", function() {
-    var url = "http://localhost:8080/addTwoNumbers/3/5";
-    it("returns status 200 to check if api works", function(done) {
+describe("Landing Page Test Cases", function() {
+    var url = "http://localhost:8080";
+    it("returns status 200 if page loads", function(done) {
         request(url, function(error, response, body) {
             expect(response.statusCode).to.equal(200);
             done()
           });
     });
-    it("returns statusCode key in body to check if api give right result should be 200", function(done) {
+    
+    /*it("returns statusCode key in body to check if api give right result should be 200", function(done) {
         request(url, function(error, response, body) {
-            body = JSON.parse(body)
+            body = $.statusCode('#register.primary-button');
+            console.log(body)
             expect(body.statusCode).to.equal(200);
             done()
           });
@@ -36,29 +38,5 @@ describe("Add Two Numbers", function() {
         expect(body.result).to.not.equal(15);
         done()
       });
-});
-  });
-
-  describe("Add Two strings", function() {
-    var url = "http://localhost:8080/addTwoNumbers/a/b";
-    it("should not returns status 200", function(done) {
-        request(url, function(error, response, body) {
-            expect(response.statusCode).to.equal(200);
-            done()
-          });
-    });
-    it("returns statusCode key in body to check if api gives right result should be 400", function(done) {
-        request(url, function(error, response, body) {
-            body = JSON.parse(body)
-            expect(body.statusCode).to.equal(400);
-            done()
-          });
-    });
-    it("returns the result as null", function(done) {
-        request(url, function(error, response, body) {
-            body = JSON.parse(body)
-            expect(body.result).to.be.a('null');
-            done()
-          });
-    });
+});*/
   });
