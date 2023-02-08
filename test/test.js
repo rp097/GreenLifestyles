47e6ff83be1fca5b25d2e4d6a1cea85e73c445e6
emilychinhh <emilyhui3823@gmail.com>
2023-02-09 09:44:42 +1100

    Test Dashboard

--- test/test.js
+++ test/test.js
@@ -9,6 +9,23 @@ describe("Landing Page Test Cases", function() {
             done()
           });
     });
+
+describe("Dashboard Page Test Cases", function() {
+      var url = "http://localhost:8080/event.html";
+      it("returns status 200 if success", function(done) {
+        request(url, function(error, response, body) {
+          expect(response.statusCode).to.equal(200);
+          done()
+          });
+      });
+      it("returns status 404 if error", function(done) {
+        request(url, function(error, response, body) {
+          expect(response.statusCode).to.not.equal(404);
+          done()
+          });
+      });
+});
+      
     
     /*it("returns statusCode key in body to check if api give right result should be 200", function(done) {
         request(url, function(error, response, body) {
