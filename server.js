1264d877cbd87fdbe80a44d2ffa3a4b7a4d873e4
rp097 <rahulprem90@gmail.com>
2023-02-08 17:02:51 +1100

    Established Testing Framework

--- server.js
+++ server.js
@@ -50,6 +50,21 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
 
+app.get("/test", function (request, response) {
+  var user_name = request.query.user_name;
+  response.end("Hello " + user_name + "!");
+});
+
+app.get('/addTwoNumbers/:firstNumber/:secondNumber', function(req,res,next){
+  var firstNumber = parseInt(req.params.firstNumber) 
+  var secondNumber = parseInt(req.params.secondNumber)  
+  var result = firstNumber + secondNumber || null  
+  if(result == null) {  
+    res.json({result: result, statusCode: 400}).status(400)  
+  }  
+  else { res.json({result: result, statusCode: 200}).status(200) }  
+})
+
 app.get("/", function (req, res) {
   res.sendFile(path.join(__dirname, "/index.html"));
 });
