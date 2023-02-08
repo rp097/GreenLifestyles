6c5f0fc0142a1366a998a43429cc7423e943325a
rp097 <rahulprem90@gmail.com>
2023-02-09 09:29:01 +1100

    Test Cases

--- test/test.js
+++ test/test.js
@@ -12,7 +12,7 @@ describe("Landing Page Test Cases", function() {
         request(url, function(error, response, body) {
             expect(response.statusCode).to.equal(200);
             done()
-          });
+          });     
     });
     
     /*it("returns statusCode key in body to check if api give right result should be 200", function(done) {
@@ -22,35 +22,28 @@ describe("Landing Page Test Cases", function() {
             expect(body.statusCode).to.equal(200);
             done()
           });
-    });
-    it("returns the result as number", function(done) {
+    });*/
+    var url = "http://localhost:8080";
+    it("returns status 404 if error", function(done) {
         request(url, function(error, response, body) {
-            body = JSON.parse(body)
-            expect(body.result).to.be.a('number');
+            expect(response.statusCode).to.not.equal(404);
             done()
           });
+               
     });
-    it("returns the result equal to 8", function(done) {
-      request(url, function(error, response, body) {
-          body = JSON.parse(body)
-          expect(body.result).to.equal(8);
-          done()
-        });
-  });
-  it("returns the result not equal to 15", function(done) {
-    request(url, function(error, response, body) {
-        body = JSON.parse(body)
-        expect(body.result).to.not.equal(15);
-        done()
-      });
-});*/
   });
 
   describe("Sign Up Page Test Cases", function() {
     var url = "http://localhost:8080/signupoptions.html";
-    it("returns status 200 if page loads", function(done) {
+    it("returns status 200 if success", function(done) {
         request(url, function(error, response, body) {
             expect(response.statusCode).to.equal(200);
+            done()
+          });
+    });
+    it("returns status 404 if error", function(done) {
+        request(url, function(error, response, body) {
+            expect(response.statusCode).to.not.equal(404);
             done()
           });
     });
