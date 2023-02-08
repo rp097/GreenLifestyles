a5c5d54d1c8f6a67ddacf595c96c62a15c1008b7
lewisyau <101070737+lewisyau@users.noreply.github.com>
2023-02-09 10:39:09 +1100

    login page test case

--- test/test.js
+++ test/test.js
@@ -66,6 +66,21 @@ describe("Dashboard Page Test Cases", function() {
     });
 });
 
+describe("Login Page Test Cases", function() {
+  var url = "http://localhost:8080/login.html";
+  it("returns status 200 if success", function(done) {
+    request(url, function(error, response, body) {
+      expect(response.statusCode).to.equal(200);
+      done()
+    });
+  });
+  it("returns status 404 if error", function(done) {
+    request(url, function(error, response, body) {
+      expect(response.statusCode).to.not.equal(404);
+      done()
+    });
+  });
+});
 
 
 
